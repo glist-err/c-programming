1eb62b78b7a2c76fd5e23dc440301ca7a4d85664
Alex Kaz <you@example.com>
2025-10-06 16:58:55 +0700

    ex4

--- src/exercise4.c
+++ src/exercise4.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
-    printf("Hello World!");
+    float X = atof(argv[1]);
+    int K = atoi(argv[2]);
+    
+    printf("Enter 10 numbers: ");
+    
+    float array[10];
+
+    for (int i = 0; i < 10; ++i) {
+        scanf("%f", &array[i]);
+    }
+    
+    for (int i = 9; i > K; --i) {
+        array[i] = array[i-1];
+    }
+
+    array[K] = X;
+
+    printf("New array:\n");
+    for(int i = 0; i < 10; ++i) {
+        printf("%.0f ", array[i]);
+    }
+
     return 0;
 }
