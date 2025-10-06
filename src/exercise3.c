8accf3a29743dad72b6325d908d999b639b72232
Alex Kaz <you@example.com>
2025-10-06 17:40:07 +0700

    ex5

--- src/exercise3.c
+++ src/exercise3.c
@@ -4,13 +4,13 @@
 #define N 10
 
 int main(int argc, char** argv) {
-    int array[N];
-    int new_arr[N];
+    float array[N];
+    float new_arr[N];
     int sdvig = atoi(argv[1]);
 
     printf("Enter 10 numbers: ");
     for (int i = 0; i < N; ++i) {
-        scanf("%d", &array[i]);
+        scanf("%f", &array[i]);
     }
 
     for (int i = 0; i < N; ++i) {
@@ -19,7 +19,7 @@ int main(int argc, char** argv) {
 
     printf("New array:\n");
     for (int i = 0; i < N; ++i) {
-        printf("%d ", new_arr[i]);
+        printf("%f ", new_arr[i]);
     }
 
     return 0;
--- src/exercise5.c
+++ src/exercise5.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-    printf("Hello World!");
+    // Matrix A
+    printf("Enter matrix A: ");
+    
+    float arrayA[10][3];
+
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            scanf("%f", &arrayA[i][j]);
+        }
+    }
+
+    // MatrixB
+    printf("Enter matrix B: ");
+    
+    float arrayB[3][10];
+
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            scanf("%f", &arrayB[i][j]);
+        }
+    }
+
+    // A * B
+    float arrC[10][10];
+
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            arrC[i][j] = 0;
+
+            for (int k = 0; k < 3; ++k) {
+                arrC[i][j] += arrayA[i][k] * arrayB[k][j];
+            }
+        }
+    }
+    
+    printf("Array C: ");
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            printf("%.0f ", arrC[i][j]);
+        }
+    }
+
     return 0;
 }
